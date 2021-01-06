63a702021550d5ff72e886dcd201004175aa1be4
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-06 15:48:17 +0530

    XAMARIN-35863 - Sample and screenshot added

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/Behavior/CalendarPageBehavior.cs
@@ -0,0 +1,55 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class CalendarPageBehavior : Behavior<ContentPage>
+    {
+        SfCalendar calendar;
+        CalendarEventCollection appointments;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.calendar = bindable.FindByName<SfCalendar>("calendar");
+            this.WireEvents();
+        }
+        private void WireEvents()
+        {
+            this.calendar.OnMonthCellLoaded += Calendar_OnMonthCellLoaded;
+        }
+        private void Calendar_OnMonthCellLoaded(object sender, MonthCellLoadedEventArgs args)
+        {
+            var viewModel = (calendar.BindingContext as ViewModel);
+            viewModel.Date = args.Date;
+            viewModel.IsAppointment = false;
+            viewModel.AppointmentCount = "";
+
+            appointments = calendar.DataSource as CalendarEventCollection;
+
+            for (int i = 0; i < appointments.Count; i++)
+            {
+                var appointment = appointments[i];
+                if (args.Date.Date == appointment.StartTime.Date)
+                {
+                    // Count Starts from 0
+                    viewModel.AppointmentCount = (i + 1).ToString();
+                    viewModel.IsAppointment = true;
+                }
+            }
+            args.CellBindingContext = viewModel;
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.UnWireEvents();
+        }
+        private void UnWireEvents()
+        {
+            this.calendar.OnMonthCellLoaded -= Calendar_OnMonthCellLoaded;
+        }
+    }
+}
+
---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/ViewModel/ViewModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Syncfusion.SfCalendar.XForms;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public CalendarEventCollection Appointments { get; set; }
+        public DateTime Date { get; set; }
+        public string AppointmentCount { get; set; }
+        public bool IsAppointment { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ViewModel()
+        {
+            this.AddAppointments();
+        }
+        private void AddAppointments()
+        {
+            Appointments = new CalendarEventCollection();
+            var random = new Random();
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-5).AddHours(10),
+                EndTime = DateTime.Now.Date.AddDays(-5).AddHours(12),
+                Subject = "Meeting",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-9).AddHours(10),
+                EndTime = DateTime.Now.Date.AddDays(-9).AddHours(12),
+                Subject = "Planning",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-9).AddHours(15),
+                EndTime = DateTime.Now.Date.AddDays(-9).AddHours(17),
+                Subject = "Execution",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-15).AddHours(10),
+                EndTime = DateTime.Now.Date.AddDays(-15).AddHours(12),
+                Subject = "Meeting",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-15).AddHours(13),
+                EndTime = DateTime.Now.Date.AddDays(-15).AddHours(15),
+                Subject = "Planning",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-15).AddHours(15),
+                EndTime = DateTime.Now.Date.AddDays(-15).AddHours(17),
+                Subject = "Execution",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-4).AddHours(10),
+                EndTime = DateTime.Now.Date.AddDays(-4).AddHours(12),
+                Subject = "Scrum Meeting",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-4).AddHours(13),
+                EndTime = DateTime.Now.Date.AddDays(-4).AddHours(15),
+                Subject = "Product Planning",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-4).AddHours(15),
+                EndTime = DateTime.Now.Date.AddDays(-4).AddHours(17),
+                Subject = "Execution",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-4).AddHours(17),
+                EndTime = DateTime.Now.Date.AddDays(-4).AddHours(18),
+                Subject = "Review Meeting",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-24).AddHours(09),
+                EndTime = DateTime.Now.Date.AddDays(-24).AddHours(10),
+                Subject = "Scrum Meeting",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-28).AddHours(11),
+                EndTime = DateTime.Now.Date.AddDays(-28).AddHours(13),
+                Subject = "Product Planning",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-28).AddHours(15),
+                EndTime = DateTime.Now.Date.AddDays(-28).AddHours(17),
+                Subject = "Plan Execution",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-20).AddHours(17),
+                EndTime = DateTime.Now.Date.AddDays(-20).AddHours(18),
+                Subject = "Review Meeting",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-20).AddHours(18),
+                EndTime = DateTime.Now.Date.AddDays(-20).AddHours(19),
+                Subject = "Testing",
+                Color = Color.FromHex("#889e81")
+            });
+
+            Appointments.Add(new CalendarInlineEvent()
+            {
+                StartTime = DateTime.Now.Date.AddDays(-20).AddHours(18),
+                EndTime = DateTime.Now.Date.AddDays(-20).AddHours(19),
+                Subject = "Testing",
+                Color = Color.FromHex("#889e81")
+            });
+        }
+        private void RaisePropertyChanged(String Name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+}
